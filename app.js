52d60f56f10f53e43b567bbe1466cca410e7c95f
0xtbug <tubagus.nabil19@gmail.com>
2024-11-08 04:52:59 +0700

    feat: swagger integration

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const router = require("./src/routes"); 
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./src/config/swaggerConfig');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
