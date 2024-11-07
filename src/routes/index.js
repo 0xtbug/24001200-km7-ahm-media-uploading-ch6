ba048cecc6dae0e3b2f87475a84510f0b77894d9
0xtbug <tubagus.nabil19@gmail.com>
2024-11-08 03:48:05 +0700

    feat: add update image routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const MulterConfig = require('../libs/multerConfig');
 router.post('/images', MulterConfig.upload, ImageController.createImage);
 router.get('/images', ImageController.getAllImages);
 router.get('/images/:id', ImageController.getImage);
+router.put('/images/:id', MulterConfig.upload, ImageController.updateImage);
 
 module.exports = router;
