ca3e2408bf72e59fdc72c5609efdf5e91a33e394
0xtbug <tubagus.nabil19@gmail.com>
2024-11-09 02:13:49 +0700

    testing deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`🚀 Server is running on port ${PORT}`);
   });
 }
 
